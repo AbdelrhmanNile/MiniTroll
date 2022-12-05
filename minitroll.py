606b7e5e4b79c356ebef2dcd61a520274239bb34
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-12-05 22:19:52 +0200

    multi conditions support & input checking

--- minitroll.py
+++ minitroll.py
@@ -1,3 +1,6 @@
+from rich import print
+
+
 class MiniTroll:
     def __init__(self, kb_file) -> None:
 
@@ -48,12 +51,29 @@ class MiniTroll:
     def eval_rule(self, condition):
         if " or " not in condition and " and " in condition:
             con = condition.split(" and ")
+            for i, c in enumerate(con):
+                if "&" in c or "|" in c:
+                    con[i] = self.eval_logsym(c)
             return self.eval_logop(con, "and")
         elif " or " in condition and " and " not in condition:
             con = condition.split(" or ")
+            for i, c in enumerate(con):
+                if "&" in c or "|" in c:
+                    con[i] = self.eval_logsym(c)
             return self.eval_logop(con, "or")
         elif " or " not in condition and " and " not in condition:
             return self.eval_arth(condition.format(**self.runtime_vars))
+
+    def eval_logsym(self, con):
+        if "&" in con:
+            con = con.replace("&", " and ")
+        elif "|" in con:
+            con = con.replace("|", " or ")
+        res = self.eval_rule(con)
+        if res == True:
+            return "{_filler}=true"
+        elif res == False:
+            return "{_filler}=false"
 
     def eval_logop(self, condition, logop):
         result = []
@@ -82,7 +102,11 @@ class MiniTroll:
                     result.append(
                         self.eval_logop(condition[i].format(**self.runtime_vars), "or")
                     )
-        return self.logops[logop](*result)
+        print(logop, result)
+        if logop == "or":
+            for i in result:
+                print(type(i))
+        return self.logops[logop](result)
 
     def eval_arth(self, con):
         lhs, op, rhs = con.split()
@@ -103,11 +127,11 @@ class MiniTroll:
     def lt(self, lhs, rhs):
         return lhs < rhs
 
-    def OR(*args):
-        return any(args)
+    def OR(self, to_or):
+        return any(to_or)
 
-    def AND(*args):
-        return all(args)
+    def AND(self, to_and):
+        return all(to_and)
 
     def is_number(self, s):
         try:
@@ -129,24 +153,30 @@ class MiniTroll:
             return False
 
     def inference(self):
-        self.runtime_vars = {}
+        self.runtime_vars = {"_filler": True}
         for i, key in self.keys.items():
             usr_in = input(f"{key} {self.hints[i]}: ")
             if usr_in not in self.hints[i]:
                 if self.hints[i] == "(any)":
-                    self.runtime_vars[i] = usr_in
+                    pass
                 elif self.hints[i] == "(number)":
-                    print("Please enter a number")
-                    if self.is_number(usr_in):
-                        self.runtime_vars[i] = float(usr_in)
-                    else:
-                        print("Please enter valid a number")
-                        return
+                    while not self.is_number(usr_in):
+                        if not self.is_number(usr_in):
+                            print("[bold red]Please enter a valid number")
+                        usr_in = input(f"{key} {self.hints[i]}: ")
                 elif self.hints[i] == "(bool)":
-                    if self.is_bool(usr_in):
-                        self.runtime_vars[i] = self.str_to_bool(usr_in)
+                    while not self.is_bool(usr_in):
+                        if not self.is_bool(usr_in):
+                            print("[bold red]Please enter a boolean value (true/false)")
+                        usr_in = input(f"{key} {self.hints[i]}: ")
+                else:
+                    while usr_in not in self.hints[i]:
+                        print(f"[bold red]Please enter one of {self.hints[i]}")
+                        usr_in = input(f"{key} {self.hints[i]}: ")
+                self.runtime_vars[i] = usr_in
             elif usr_in in self.hints[i]:
                 self.runtime_vars[i] = usr_in
+
         results = []
         for i, rule in enumerate(self.rules):
             res = self.parse_rule(rule)
@@ -154,7 +184,7 @@ class MiniTroll:
             res_val = list(res.values())[0]
             self.runtime_vars[res_key] = res_val
             results.append(res)
-        print("=========================================")
+        print("==================|RESULTS|=======================")
 
         for i in results:
             print(i)
